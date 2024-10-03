f9c36e8dd8f1c588c34e5b4cdbb9f8b3614753cc
PEYTON-150 <78567261+PEYTON-150@users.noreply.github.com>
2024-10-03 09:45:39 -0400

    Transfer
    From student to personal account

---
+++ SE361_GitHub_Demo-master/SE361_GitHub_Demo-master/WebBrowserGitHubDemo/WebBrowserGitHubDemo/WebBrowserGUI.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WebBrowserGitHubDemo
+{
+    partial class WebBrowserGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnGO = new System.Windows.Forms.Button();
+            this.wbrDisplay = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter URL:";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(132, 41);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(266, 20);
+            this.txtURL.TabIndex = 1;
+            // 
+            // btnGO
+            // 
+            this.btnGO.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGO.Location = new System.Drawing.Point(404, 35);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(75, 34);
+            this.btnGO.TabIndex = 2;
+            this.btnGO.Text = "GO";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // wbrDisplay
+            // 
+            this.wbrDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.wbrDisplay.Location = new System.Drawing.Point(-3, 75);
+            this.wbrDisplay.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbrDisplay.Name = "wbrDisplay";
+            this.wbrDisplay.Size = new System.Drawing.Size(719, 360);
+            this.wbrDisplay.TabIndex = 3;
+            // 
+            // WebBrowserGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 432);
+            this.Controls.Add(this.wbrDisplay);
+            this.Controls.Add(this.btnGO);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Name = "WebBrowserGUI";
+            this.Text = "Web Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.WebBrowser wbrDisplay;
+    }
+}
+
